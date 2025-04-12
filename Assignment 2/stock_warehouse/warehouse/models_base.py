93064b0c8696dac18eb3eee8f21121a1daee155c
Iqbal Fadhil <halo@iqbalfadhil.com>
2025-04-12 20:43:28 +0700

    Tested Base Model inheritance to Items

--- Assignment 2/stock_warehouse/warehouse/models_base.py
+++ Assignment 2/stock_warehouse/warehouse/models_base.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    is_deleted = models.BooleanField(default=False, null=True, blank=True)
 
     class Meta:
         abstract = True
